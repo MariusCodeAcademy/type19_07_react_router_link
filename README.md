02c3bc00721da80b49707936ecc58ee4416cacf9
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-01-18 10:36:49 +0200

    install router + emty pages + Browserrouter

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Steps
+
+1. `npm i react-router-dom`
+2. apjuosti su BrowserRouter musu App
+
+```js
+import { BrowserRouter } from 'react-router-dom';
+<BrowserRouter>
+  <App />,
+</BrowserRouter>,
+```
--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,15 @@
-import { useState } from 'react';
 import './styles/App.css';
 import AboutPage from './pages/AboutPage';
 
 export default function App() {
-  const [currentPage, setCurrentPage] = useState(1);
-
   return (
     <div className='App container'>
-      <h1>Hello, world!</h1>
       <nav className='flex'>
-        <a onClick={() => setCurrentPage(1)} href='#'>
-          Home
-        </a>
-        <a onClick={() => setCurrentPage(2)} href='#'>
-          About
-        </a>
-        <a onClick={() => setCurrentPage(3)} href='#'>
-          Contact
-        </a>
+        <a href='#'>Home</a>
+        <a href='#'>About</a>
+        <a href='#'>Contact</a>
       </nav>
       <hr />
-
-      {currentPage === 1 && <h2>Home</h2>}
-      {currentPage === 2 && <AboutPage />}
-      {currentPage === 3 && <h2>Contact Us</h2>}
     </div>
   );
 }
