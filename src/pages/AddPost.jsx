2554450c3b10772d70eb78b0913c71cfa2dc0d52
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-01-18 11:57:03 +0200

    update object state

--- src/pages/AddPost.jsx
+++ src/pages/AddPost.jsx
@@ -1,6 +1,27 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function AddPost() {
+  const [inputsObj, setInputsObj] = useState({
+    title: '',
+    author: 'James Band',
+  });
+
+  function handleTitleInput(e) {
+    const reikme = e.target.value;
+    // paimam viska kas yra state ir pridedam kas pasikeite
+    setInputsObj({ ...inputsObj, title: reikme });
+  }
+
+  function handleAuthorInput(e) {
+    const reikme = e.target.value;
+    // paimam viska kas yra state ir pridedam kas pasikeite
+    setInputsObj({ ...inputsObj, author: reikme });
+  }
+
+  const arr = [];
+  arr.push(1);
+
   function handleNewPostFormSubmit(e) {
     e.preventDefault();
 
@@ -10,16 +31,27 @@ export default function AddPost() {
   return (
     <div className='container'>
       <h1>Create post</h1>
+      <p>title: {inputsObj.title}</p>
       <Link to={'/'}>Back to home</Link>
 
       <form onSubmit={handleNewPostFormSubmit} className='add-post-form'>
         <label>
           <span>Title</span>
-          <input type='text' placeholder='Title' />
+          <input
+            onChange={handleTitleInput}
+            value={inputsObj.title}
+            type='text'
+            placeholder='Title'
+          />
         </label>
         <label>
           <span>Author</span>
-          <input type='text' placeholder='Author' />
+          <input
+            onChange={handleAuthorInput}
+            value={inputsObj.author}
+            type='text'
+            placeholder='Author'
+          />
         </label>
         <label>
           <span>Body</span>
